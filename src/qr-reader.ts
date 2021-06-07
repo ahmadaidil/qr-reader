d1a77c405b719992b128583e3d6694d80aaecdb5
Ahmad Aidil <ahmdaidil@gmail.com>
2021-06-07 12:26:43 +0700

    handle unwanted error: trycatch block

--- src/qr-reader.ts
+++ src/qr-reader.ts
@@ -10,51 +10,55 @@ class QRReader extends (EventEmitter as new () => TypedEmitter<QRReader.Events>)
   constructor({ vendorId, productId, serialNumber }: { vendorId: number, productId: number, serialNumber: string }) {
     super()
     setTimeout(() => {
-      const devices = HID.devices()
+      try {
+        const devices = HID.devices()
 
-      const getDevice = (device: HID.Device) => {
-        return device.vendorId === vendorId && device.productId === productId && device.serialNumber === serialNumber
-      }
-
-      if (os.platform() === 'linux') {
-        const deviceList = devices.filter(getDevice)
-        if (deviceList.length) {
-          this.reader = new HID.HID(deviceList[0].path!)
+        const getDevice = (device: HID.Device) => {
+          return device.vendorId === vendorId && device.productId === productId && device.serialNumber === serialNumber
         }
-      } else {
-        const deviceInfo = devices.find(getDevice)
 
-        if (deviceInfo) {
-          this.reader = new HID.HID(deviceInfo.path!)
+        if (os.platform() === 'linux') {
+          const deviceList = devices.filter(getDevice)
+          if (deviceList.length) {
+            this.reader = new HID.HID(deviceList[0].path!)
+          }
+        } else {
+          const deviceInfo = devices.find(getDevice)
+
+          if (deviceInfo) {
+            this.reader = new HID.HID(deviceInfo.path!)
+          }
         }
-      }
 
-      if (this.reader) {
-        this.emit('open', `QR device with PID ${productId}, VID ${vendorId} and SN ${serialNumber} is now open!`)
-        this.reader.on('data', data => {
-          const code = Buffer.from([data[2]]).toString('hex')
-          const signal = Buffer.from([data[0]]).toString('hex')
-          const key = KEYMAP[code]
+        if (this.reader) {
+          this.emit('open', `QR device with PID ${productId}, VID ${vendorId} and SN ${serialNumber} is now open!`)
+          this.reader.on('data', data => {
+            const code = Buffer.from([data[2]]).toString('hex')
+            const signal = Buffer.from([data[0]]).toString('hex')
+            const key = KEYMAP[code]
 
-          const thesignal = (signal == '00') ? 0 : 1
+            const thesignal = (signal == '00') ? 0 : 1
 
-          let char = ''
+            let char = ''
 
-          if (code !== '0' && key) {
-            char = key['chars'][thesignal]
-            this.input += key['chars'][thesignal]
-          }
+            if (code !== '0' && key) {
+              char = key['chars'][thesignal]
+              this.input += key['chars'][thesignal]
+            }
 
-          if (this.input && char === '\n') {
-            this.emit('data', this.input.trim())
-            this.input = ''
-          }
-        })
-        this.reader.on('error', err => {
-          this.emit('error', err)
-        })
-      } else {
-        this.emit('error', new Error(`QR device with PID ${productId}, VID ${vendorId} and SN ${serialNumber} not found!`))
+            if (this.input && char === '\n') {
+              this.emit('data', this.input.trim())
+              this.input = ''
+            }
+          })
+          this.reader.on('error', err => {
+            this.emit('error', err)
+          })
+        } else {
+          this.emit('error', new Error(`QR device with PID ${productId}, VID ${vendorId} and SN ${serialNumber} not found!`))
+        }
+      } catch(err) {
+        this.emit('error', new Error(`QR device with PID ${productId}, VID ${vendorId} and SN ${serialNumber} has error: ${err.message}`))
       }
     }, 500);
   }
